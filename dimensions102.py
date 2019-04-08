895117132e5a47acc268a56e722e1ac8495263fa
aravindsr <aravind.sraamkumar@gmail.com>
2019-04-09 03:28:29 +0800

    Update dimensions102.py

--- dimensions102.py
+++ dimensions102.py
@@ -11,10 +11,6 @@ import numpy as np
 import pandas as pd
 
 
-doidata = pd.read_csv('universitylist_4.txt', header=None,delimiter=None,dtype=object)
-print(len(doidata))
-#print(univdata[0][1])
-#print(univdata.shape)
 
 op = pd.DataFrame([])
 
@@ -38,32 +34,34 @@ headers = {
     'Authorization': "JWT " + resp.json()['token']
 }
 
+doidata = pd.read_csv('HWC_DOI.txt', header=None,delimiter=None,dtype=object)
+print(len(doidata))
 
-for i in range(2000):
-    print(univdata[0][i])
-    univs = api.search_users(univdata[0][i])
-    univslen=len(univs)
-    print('Retrieved list of users is',univslen,univdata[0][i])
-    if(univslen>0):
-        #print(str(univs[0].id)+'\t'+str(univs[0].screen_name)+'\t'+str(univs[0].name)+'\t'+str(univs[0].location)+'\t'+str(univs[0].followers_count)+'\t'+str(univs[0].friends_count)+'\t'+str(univs[0].url)+'\t'+univs[0].description)
-        f.write('\n' + str(univs[0].id)+'\t'+str(univs[0].screen_name)+'\t'+str(univs[0].name)+'\t'+str(univs[0].location)+'\t'+str(univs[0].followers_count)+'\t'+str(univs[0].friends_count)+'\t'+str(univs[0].statuses_count)+'\t'+str(univs[0].url)+'\t'+univs[0].description)
-        f.flush()
-f.close()    
+f = open('opdoifor.txt','a',encoding="utf-8")
 
+for i in range(729):
+    print(doidata[0][i])
+    resp = requests.post(
+    'https://app.dimensions.ai/api/dsl.json',
+    data='search publications where doi=\"'+doidata[0][i]+'\" return publications [FOR]',
+    #data='search publications where doi=\"'+doidata[0][i]+'\" return publications',
+   #data='search publications  for \"health coaching\" return publications',
+    headers=headers)
+    result=resp.json()
+    result_new=json.dumps(result)
+    print(result_new)
+    f.write('\n' + doidata[0][i]+'\t'+result_new)
+    f.flush()
+
+f.close()
 
 
 
 #print(headers)
 #   Execute DSL query.
-resp = requests.post(
-    'https://app.dimensions.ai/api/dsl.json',
-   #data='search publications where doi=\"10.1007/s13142-015-0313-4\" return publications [FOR]',
-   data='search publications where doi=\"10.1007/s13142-015-0313-4\" return publications',
-   #data='search publications  for \"health coaching\" return publications',
-    headers=headers)
-result=resp.json()
-result_new=json.dumps(result)
+
+
 #result_new=result_new.replace('\'','"')
 #result_new=result_new.replace('None','"None"')
 #   Display raw result
-print(result_new)
+
